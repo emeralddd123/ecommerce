4d91b93fc39f856aac3e39493ff460e17532c087
emeralddd123 <93847541+emeralddd123@users.noreply.github.com>
2023-01-26 15:53:14 +0100

    newslettrer subscription fixed

--- core/views.py
+++ core/views.py
@@ -302,29 +302,24 @@ def subscribe(request):
                 "You must type legit name and email to subscribe to a Newsletter",
             )
             return redirect("/")
-
-        if get_user_model().objects.filter(email=email).first():
-            messages.error(
-                request,
-                f"Found registered user with associated {email} email. You must login to subscribe or unsubscribe.",
-            )
-            return redirect(request.META.get("HTTP_REFERER", "/"))
-
         subscribe_user = SubscribedUser.objects.filter(email=email).first()
-        if subscribe_user:
-            messages.error(request, f"{email} email address is already subscriber.")
-            return redirect(request.META.get("HTTP_REFERER", "/"))
-
-        try:
-            validate_email(email)
-        except ValidationError as e:
-            messages.error(request, e.messages[0])
-            return redirect("/")
-
-        subscribe_model_instance = SubscribedUser()
-        subscribe_model_instance.email = email
-        subscribe_model_instance.save()
-        messages.success(
+        if get_user_model().objects.filter(email=email).first() and not subscribe_user:
+            subscribe_model_instance = SubscribedUser()
+            subscribe_model_instance.email = email
+            subscribe_model_instance.save()
+            messages.success(
             request, f"{email} email was successfully subscribed to our newsletter!"
         )
+            return redirect(request.META.get("HTTP_REFERER", "/"))
+        elif subscribe_user:
+            messages.error(request, f"{email} email address is already subscriber.")
+            return redirect(request.META.get("HTTP_REFERER", "/"))
+        
+        elif not get_user_model().objects.filter(email=email).first():
+            messages.success(
+            request, f"{email} email is not valid registered, Please register first!"
+        )
+            return redirect(request.META.get("HTTP_REFERER", "/"))
+        
+        
         return redirect(request.META.get("HTTP_REFERER", "/"))
